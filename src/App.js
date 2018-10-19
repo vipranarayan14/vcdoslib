660936a21b34e7f025dbba78ff4ea2fd9a45ae3b
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-10-19 23:17:27 +0530

    add book list.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {parse} from 'papaparse';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+
+    const config = {
+      header: true,
+      download: true,
+      complete: results => {
+        this.setState({data: results});
+      }
+    };
+
+    parse(
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y' +
+          '78r7Y488E/pub?gid=0&single=true&output=csv',
+      config
+    );
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="data-table">
+          {
+            this.state.data
+              ? this.state.data.data.map((row, i) => (
+                <div className="row" key={i}>
+                  <span className="accno">{row['Accesion No.']}</span>
+                  <span className="title">{row['Book Title']}</span>
+                  <span className="author">{row['Author']}</span>
+                  <span className="rack">{row['Rack No.']}</span>
+                  <span className="subject">{row['Classification']}</span>
+                </div>
+              ))
+              : null
+          }
+        </div>
       </div>
     );
   }
+
 }
 
 export default App;
