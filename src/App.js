2ef961b5a5e513ce600b61eb1b72c6afe5f411bd
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-10-20 18:45:41 +0530

    add support for fuzzy searching through books.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,75 @@
 import React, {Component} from 'react';
-import {BookList} from './components/BookList'
+import {BookList} from './components/BookList';
+import {SearchBox} from './components/SearchBox';
 
 import {parse} from 'papaparse';
+import * as Fuse from 'fuse.js';
+
 import './App.css';
+
+const options = {
+  distance: 100,
+  findAllMatches: true,
+  keys: ['BookTitle', 'Author', 'AccesionNo', 'RackNo'],
+  location: 0,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  shouldSort: true,
+  threshold: 0.5
+};
+
+const parseCSV = () => new Promise((resolve, reject) => {
+  parse(
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y' +
+        '78r7Y488E/pub?gid=0&single=true&output=csv',
+    {
+      header: true,
+      download: true,
+      complete: resolve
+    }
+  );
+});
+
+let allbooks = [],
+  fuse;
 
 class App extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-      books: []
-    }
+      searchQuery: '',
+      searchResults: []
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
 
-    const config = {
-      header: true,
-      download: true,
-      complete: results => {
-        this.setState({books: results.data});
-      }
-    };
-
-    parse(
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y' +
-          '78r7Y488E/pub?gid=0&single=true&output=csv',
-      config
-    );
+    parseCSV().then(results => {
+      allbooks = results.data;
+      fuse = new Fuse(allbooks, options);
+    });
 
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      searchQuery: e.target.search.value,
+      searchResults: fuse.search(e.target.search.value)
+    });
+  }
+
   render() {
+
     return (
       <div className="App">
-        <BookList books={this.state.books} />
+        <SearchBox handleSubmit={this.handleSubmit}/>
+        <BookList searchResults={this.state.searchResults} searchQuery={this.state.searchQuery}/>
       </div>
     );
   }
---
+++ src/components/SearchBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import './SearchBox.css';
+
+export const SearchBox = ({handleSubmit}) => (
+
+  <div className="SearchBox">
+    <form onSubmit={handleSubmit}>
+      <input name="search" type="search"/>
+      <input type="submit" value="Search"/>
+    </form>
+  </div>
+
+);
+
+SearchBox.propTypes = {
+  handleSubmit: PropTypes.func.isRequired
+}
