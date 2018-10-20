fd01ce88ea9499576e06ae05dfdcbee43876dafd
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-10-20 23:31:37 +0530

    change 'booklist' to 'BookList'. use table for display books to ease alignment and styling. add conditional display to book details.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,20 +5,35 @@ import './BookList.css';
 
 export const BookList = ({searchResults}) => !!searchResults.length && (
 
-  <div className="booklist">
-    {
-      searchResults.map((row, i) => (
-        <div className="row" key={i}>
-          <span className="accno">{row['AccesionNo']}</span>
-          <span className="title">{row['BookTitle']}</span>
-          <span className="author">{row['Author']}</span>
-          <span className="rack">{row['RackNo']}</span>
-          <span className="subject">{row['Classification']}</span>
-        </div>
-      ))
-    }
+  <div className="BookList">
+    <table>
+      <tbody>
+        {
+          searchResults.map((row, i) => (
+            <tr
+              className={"book" + (
+                row['RackNo'] === 'R-0-Z'
+                  ? " missing"
+                  : ""
+              )}
+              key={i}>
+              <td>
+                {!!row['AccesionNo'] && <div className="accno">{row['AccesionNo']}</div>}
+              </td>
+              <td>
+                <div className="title">{row['BookTitle']}</div>
+                {!!row['Author'] && <div className="author">by {row['Author']}</div>}
+              </td>
+              <td>
+                {(!!row['RackNo'] && row['RackNo'] !== 'R-0-Z') && <div className="rack">{row['RackNo']}</div>}
+                {!!row['Classification'] && <div className="subject">{row['Classification']}</div>}
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   </div>
-
 );
 
 BookList.propTypes = {
