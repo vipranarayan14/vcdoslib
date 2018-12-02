70c97a12ee103431516014b34ed655297287b93c
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-12-02 13:39:17 +0530

    fix incorrect usage of propTypes.

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './ScrollToTop.css';
+import "./ScrollToTop.css";
 
 const handleScrollToTopClick = () => {
-
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-
 };
 
-export const ScrollToTop = ({isTop}) => !isTop && (
+export const ScrollToTop = ({ isTop }) =>
+  !isTop && (
+    <div className="ScrollToTop" onClick={handleScrollToTopClick}>
+      Top
+    </div>
+  );
 
-  <div className="ScrollToTop" onClick={handleScrollToTopClick}>Top</div>
-
-);
-
-React.PropTypes = {
+ScrollToTop.propTypes = {
   isTop: PropTypes.bool.isRequired
 };
