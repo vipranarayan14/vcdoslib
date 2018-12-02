3ff07e7f9d08f8a52600d00bc00b18c14eb03f3a
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-12-02 14:02:58 +0530

    remove `console.log` prettify.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 /* libraries */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 /* components */
-import {Results} from './components/Results';
-import {ScrollToTop} from './components/ScrollToTop';
-import {SearchBox} from './components/SearchBox';
+import { Results } from './components/Results';
+import { ScrollToTop } from './components/ScrollToTop';
+import { SearchBox } from './components/SearchBox';
 
 /* modules */
-import {initFuse} from './modules/init-fuse';
-import {parseCSV} from './modules/parse-csv';
+import { initFuse } from './modules/init-fuse';
+import { parseCSV } from './modules/parse-csv';
 
 /* stylesheets */
 import './App.css';
@@ -17,7 +17,6 @@ let allbooks = [],
   fuse;
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
@@ -35,22 +34,19 @@ class App extends Component {
   }
 
   componentDidMount() {
-
     parseCSV().then(results => {
       allbooks = results.data;
       fuse = initFuse(allbooks);
     });
 
     window.addEventListener('scroll', this.handleScroll);
-
   }
 
   handleScroll() {
+    const isTop =
+      document.body.scrollTop < 250 && document.documentElement.scrollTop < 250;
 
-    const isTop = document.body.scrollTop < 250 && document.documentElement.scrollTop < 250;
-
-    this.setState({isTop});
-
+    this.setState({ isTop });
   }
 
   handleSubmit(e) {
@@ -59,7 +55,9 @@ class App extends Component {
     const searchQuery = e.target.search.value;
     const fuseResults = fuse.search(searchQuery);
 
-    const firstPartialMatchIndex = fuseResults.findIndex(result => result.score > 0.05);
+    const firstPartialMatchIndex = fuseResults.findIndex(
+      result => result.score > 0.05
+    );
 
     /* using `firstPartialMatchIndex` directly will not give unexpected results. Tested! */
     const exactMatches = fuseResults.slice(0, firstPartialMatchIndex);
@@ -72,26 +70,27 @@ class App extends Component {
       partialMatches
     };
 
-    this.setState({searchQuery, searchResults});
+    this.setState({ searchQuery, searchResults });
   }
 
   render() {
     return (
       <div className="App">
-        {console.log(this.state.isTop)}
         <header>
           <h1>Library</h1>
           <h2>Department of Sanskrit, RKM Vivekananda College</h2>
-          <SearchBox handleSubmit={this.handleSubmit}/>
+          <SearchBox handleSubmit={this.handleSubmit} />
         </header>
         <main>
-          <Results searchResults={this.state.searchResults} searchQuery={this.state.searchQuery}/>
-          <ScrollToTop isTop={this.state.isTop}/>
+          <Results
+            searchResults={this.state.searchResults}
+            searchQuery={this.state.searchQuery}
+          />
+          <ScrollToTop isTop={this.state.isTop} />
         </main>
       </div>
     );
   }
-
 }
 
 export default App;
