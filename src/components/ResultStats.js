741643a95a613f81fe369fba4e5d0259140f62f0
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-12-02 14:07:13 +0530

    fix incorrect ResultStats logic.

--- src/components/ResultStats.js
+++ src/components/ResultStats.js
@@ -3,31 +3,25 @@ import PropTypes from 'prop-types';
 
 import './ResultStats.css';
 
-export const ResultStats = ({searchResults}) => {
-
+export const ResultStats = ({ searchResults }) => {
   const totalExactMatches = searchResults.exactMatches.length;
   const totalPartialMatches = searchResults.partialMatches.length;
 
   return (
-
     <div className="ResultStats">
-      <span>Found&nbsp;{totalExactMatches || 'no'}
-        &nbsp;exact match{
-          totalExactMatches > 1
-            ? 'es and'
-            : ' but'
-        }</span>
-      <span>&nbsp;{totalPartialMatches}&nbsp;partial match{
-          totalExactMatches > 1
-            ? 'es'
-            : ''
-        }.</span>
+      <span>
+        Found &nbsp; {totalExactMatches || 'no'}
+        &nbsp; exact match {totalExactMatches > 1 && 'es'}
+        &nbsp; {totalExactMatches < 1 ? 'but' : 'and'}
+      </span>
+      <span>
+        &nbsp; {totalPartialMatches}
+        &nbsp; partial match {totalExactMatches > 1 && 'es'}.
+      </span>
     </div>
-
   );
-
 };
 
 ResultStats.propTypes = {
   searchResults: PropTypes.object.isRequired
-}
+};
