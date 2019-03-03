fb236de4a6c1ad9fe652cd176a2bb3430ba16e2d
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2019-03-03 18:56:56 +0530

    add error handling if `parseCSV` fails.

--- src/modules/parse-csv.js
+++ src/modules/parse-csv.js
@@ -1,13 +1,15 @@
-import {parse} from 'papaparse';
+import { parse } from 'papaparse';
 
-export const parseCSV = () => new Promise((resolve, reject) => {
-  parse(
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y' +
+export const parseCSV = () =>
+  new Promise((resolve, reject) => {
+    parse(
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y' +
         '78r7Y488E/pub?gid=0&single=true&output=csv',
-    {
-      header: true,
-      download: true,
-      complete: resolve
-    }
-  );
-});
+      {
+        header: true,
+        download: true,
+        complete: resolve,
+        error: reject
+      }
+    );
+  });
