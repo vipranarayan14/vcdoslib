f944ddacf707d36f76484a67490ac68999bf24d1
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2019-03-03 20:22:02 +0530

    Use `div` for rendering search results instead of `table` to avoid: - unpredictable alignment - slow rendering - uncustomizablity (card-style, etc.) Fixes #3.

--- src/components/ResultBox.js
+++ src/components/ResultBox.js
@@ -8,35 +8,33 @@ const markMissing = rack => (rack === 'NA' ? ' missing' : '');
 export const ResultBox = ({ searchResults }) =>
   Boolean(searchResults.length) && (
     <div className="ResultBox">
-      <table>
-        <tbody>
-          {searchResults.map((result, i) => {
-            const accno = result.item['AccesionNo'];
-            const title = result.item['BookTitle'];
-            const author = result.item['Author'];
-            const subject = result.item['Classification'];
-            const rack = result.item['RackNo'];
+      {searchResults.map((result, i) => {
+        const accno = result.item['AccesionNo'];
+        const title = result.item['BookTitle'];
+        const author = result.item['Author'];
+        const subject = result.item['Classification'];
+        const rack = result.item['RackNo'];
 
-            return (
-              <tr className={'book' + markMissing(rack)} key={i}>
-                <td>{!!accno && <div className="accno">{accno}</div>}</td>
-                <td>
-                  <div className="title">{title}</div>
-                  {!!author && <div className="author">by {author}</div>}
-                  {!!subject && (
-                    <div className="subject">{`${
-                      subjects[subject]
-                    } (${subject})`}</div>
-                  )}
-                </td>
-                <td>
-                  <div className="rack">{rack}</div>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+        return (
+          <div className={'book' + markMissing(rack)} key={i}>
+            <div className="col15">
+              {!!accno && <div className="accno">{accno}</div>}
+            </div>
+            <div className="col70">
+              <div className="title">{title}</div>
+              {!!author && <div className="author">by {author}</div>}
+              {!!subject && (
+                <div className="subject">{`${
+                  subjects[subject]
+                } (${subject})`}</div>
+              )}
+            </div>
+            <div className="col15">
+              <div className="rack">{rack}</div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 ResultBox.propTypes = {
