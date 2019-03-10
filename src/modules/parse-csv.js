a48444b77bb393df3919d64cd14db48d0736866e
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2019-03-10 22:06:15 +0530

    fix improper concatination of URL string.

--- src/modules/parse-csv.js
+++ src/modules/parse-csv.js
@@ -3,8 +3,8 @@ import { parse } from 'papaparse';
 export const parseCSV = () =>
   new Promise((resolve, reject) => {
     parse(
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y' +
-        '78r7Y488E/pub?gid=0&single=true&output=csv',
+      'https://docs.google.com/spreadsheets/d/e/' +
+        '2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y78r7Y488E/pub?gid=0&single=true&output=csv',
       {
         header: true,
         download: true,
