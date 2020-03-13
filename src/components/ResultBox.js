3be11ee725d13141bd0f6e4390915943dd2ed99c
Prasanna Venkatesh <vipranarayan14@gmail.com>
2020-03-14 00:18:43 +0530

    move 'subjects' into 'modules' dir.

--- src/components/ResultBox.js
+++ src/components/ResultBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { subjects } from './subjects';
+import { subjects } from '../modules/subjects';
 
 const markMissing = rack => (rack === 'NA' ? ' missing' : '');
 
