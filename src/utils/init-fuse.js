e4d615b934731f87c5a98e9d4b2be1d5daa27ae8
Prasanna Venkatesh <vipranarayan14@gmail.com>
2020-03-23 01:44:33 +0530

    feat: add `extended` and `weighted` search support
    Extended search enables the use of unix-like search queries.
    
    Weighted search gives results in the  order of set field weightage.

--- src/utils/init-fuse.js
+++ src/utils/init-fuse.js
@@ -2,9 +2,23 @@ import * as Fuse from 'fuse.js';
 
 const options = {
   distance: 100,
+  useExtendedSearch: true,
   findAllMatches: true,
   includeScore: true,
-  keys: ['BookTitle', 'Author', 'AccesionNo', 'RackNo'],
+  keys: [
+    {
+      name: 'BookTitle',
+      weight: 0.5
+    },
+    {
+      name: 'Author',
+      weight: 0.4
+    },
+    {
+      name: 'AccesionNo',
+      weight: 0.1
+    }
+  ],
   location: 0,
   maxPatternLength: 32,
   minMatchCharLength: 1,
