941ca9de8969005968c1cb24359920597c24bd9b
Prasanna Venkatesh <vipranarayan14@gmail.com>
2020-05-03 09:59:04 +0530

    rename parseCSV to loadBooks.
    Rename parseCSV to loadBooks for readability.
    
    Remove redundant commented code.

---
+++ src/utils/load-books.js
@@ -0,0 +1,15 @@
+import { parse } from 'papaparse';
+
+export const loadBooks = () =>
+  new Promise((resolve, reject) => {
+    parse(
+      'https://docs.google.com/spreadsheets/d/e/' +
+        '2PACX-1vQ5qIl3I8n3EKIfsr5rYIhmg8eNffWrKI6zFUDYNDPUENq4KQp5SmnxKQ5OkzbxErTBX3y78r7Y488E/pub?gid=0&single=true&output=csv',
+      {
+        header: true,
+        download: true,
+        complete: resolve,
+        error: reject
+      }
+    );
+  });
